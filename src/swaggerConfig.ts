643d211a9eb3011b5c7e31016077bd4280a391a4
RodrigoHMoreira <drigohenri@gmail.com>
2024-07-07 13:49:28 -0300

    [FEAT] Ajuste de configuração para build

--- src/swaggerConfig.ts
+++ src/swaggerConfig.ts
@@ -3,7 +3,7 @@ import { Express } from "express";
 import fs from "fs";
 import YAML from "js-yaml";
 
-const swaggerFile = fs.readFileSync("src/swagger.yaml", "utf8");
+const swaggerFile = fs.readFileSync("dist/swagger.yaml", "utf8");
 const swaggerDocument: JsonObject = YAML.load(swaggerFile) as JsonObject;
 
 export const setupSwagger = (app: Express) => {
