32134e80ac12f41264d79410b08b0537c2172a49
Rodrigo <drigohenri@gmail.com>
2024-07-10 09:39:56 -0300

    *

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,22 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+
+const allowedOrigins = process.env.NODE_ENV === 'production' 
+  ? [process.env.PRODUCTION_URL]
+  : [process.env.LOCAL_URL, process.env.PRODUCTION_URL];
+
+const corsOptions = {
+  origin: function (origin: string, callback: any) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
 
 app.use("/api", routes);
 
